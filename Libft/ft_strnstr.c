91cd2b460ff94209bf82d099d4b38fb9750679ab
Thomas Stephan <tstephan@bess-f2r2s10.clusters.42paris.fr>
2024-11-05 16:54:09 +0100

    libc str fonctions

---
+++ Libft/ft_strnstr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/05 15:26:37 by marvin            #+#    #+#             */
+/*   Updated: 2024/11/05 15:31:26 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*strnstr(const char *big, const char *little, size_t len)
+{
+	int	flag;
+	int	pos;
+	while (*big && len)
+	{
+		flag = 0;
+		pos = 0;
+		while (little[pos])
+		{
+			if (big[pos] != little[pos])
+				flag++;
+			pos++;
+		}
+		if (flag == 0)
+			return ((char *) big);
+		big++;
+		len--;
+	}
+	return (NULL);
+}
--- Libft/libft.h
+++ Libft/libft.h
@@ -6,17 +6,40 @@
 /*   By: marvin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 14:07:50 by marvin            #+#    #+#             */
-/*   Updated: 2024/11/05 14:13:38 by marvin           ###   ########.fr       */
+/*   Updated: 2024/11/05 15:51:45 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
+#include <unistd.h>
+
+/*
+--------------------------------------------------------------------------------
+LIBC FONCTIONS
+--------------------------------------------------------------------------------
+*/
+
+// char fonctions
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_isalnum(int c);
 int	ft_isascii(int c);
 int	ft_isprint(int c);
+int	ft_toupper(int c);
+int	ft_tolower(int c);
+
+// str fonctions
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
+size_t ft_strlcpy(char *d, const char *s, size_t n);
+size_t ft_strlcat(char *d, const char *s, size_t n);
+char	*strnstr(const char *big, const char *little, size_t len);
+int	ft_atoi(const char *s);
+
+// mem fonctions
 
 #endif
