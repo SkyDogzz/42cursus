500b6b560c856db826fe9d7dc1addb406d1aa4c0
skydogzz <thomas.stephan@live.fr>
2024-11-17 19:18:49 +0100

    plus flag

--- ft_printf2/src/ft_nbr.c
+++ ft_printf2/src/ft_nbr.c
@@ -6,7 +6,7 @@
 /*   By: skydogzz </var/spool/mail/skydogzz>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/17 15:43:03 by skydogzz          #+#    #+#             */
-/*   Updated: 2024/11/17 18:36:31 by skydogzz         ###   ########.fr       */
+/*   Updated: 2024/11/17 19:17:46 by skydogzz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,14 +29,13 @@ size_t	ft_countcharint(int n)
 	return (len);
 }
 
-void	ft_putnbrpadded(int n, int fd, int count, char c)
+void	ft_putnbrpadded(int n, int fd, int count, char c, enum e_bool sign)
 {
 	char	num;
 
 	if (n < 0)
 	{
 		write(fd, "-", 1);
-		
 		if (n == INT_MIN)
 		{
 			write(fd, "2", 1);
@@ -47,12 +46,28 @@ void	ft_putnbrpadded(int n, int fd, int count, char c)
 			n = -n;
 		}
 	}
+	else
+	{
+		ft_putchar_fd('+', fd);
+		count--;
+	}
 	while (count--)
 		ft_putchar_fd(c, 1);
 	if (n / 10 != 0)
 		ft_putnbr_fd(n / 10, fd);
 	num = (n % 10) + '0';
 	write(fd, &num, 1);
+}
+
+void	ft_putnbrsign(int n, int fd, enum e_bool sign)
+{
+	if (!sign || n < 0)
+		ft_putnbr_fd(n, fd);
+	if (n >= 0)
+	{
+		ft_putchar_fd('+', fd);
+		ft_putnbr_fd(n, fd);
+	}
 }
 
 size_t	ft_putnbroptions_fd(int n, int fd, struct s_option options)
@@ -62,22 +77,24 @@ size_t	ft_putnbroptions_fd(int n, int fd, struct s_option options)
 	len = ft_countcharint(n);
 	if (!options.width || options.width <= len)
 	{
-		ft_putnbr_fd(n, fd);
-		return (len);
+		ft_putnbrsign(n, fd, ft_getflag(options.flag, '+'));
+		if (n < 0)
+			return (len);
+		return (len + ft_getflag(options.flag, '+'));
 	}
 	if (ft_getflag(options.flag, '-'))
 	{
-		ft_putnbr_fd(n, fd);
-		ft_addchar(' ', fd, options.width - len);
+		ft_putnbrsign(n, fd, ft_getflag(options.flag, '+'));
+		ft_addchar(' ', fd, options.width - len - ft_getflag(options.flag, '+'));
 	}
 	else if (ft_getflag(options.flag, '0'))
 	{
-		ft_putnbrpadded(n, fd, options.width - len, '0');
+		ft_putnbrpadded(n, fd, options.width - len, '0', ft_getflag(options.flag, '+'));
 	}
 	else
 	{
-		ft_addchar(' ', fd, options.width - len);
-		ft_putnbr_fd(n, fd);
+		ft_addchar(' ', fd, options.width - len - ft_getflag(options.flag, '+'));
+		ft_putnbrsign(n, fd, ft_getflag(options.flag, '+'));
 	}
 	return (options.width);
 }
