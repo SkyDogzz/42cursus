8310d3f38d0f926abe4635a6da4ca8ec8b18e6b5
SkyDogzz <thomas.stephan@live.fr>
2024-11-20 17:56:54 +0100

    78

--- ft_printf2/src/ft_d.c
+++ ft_printf2/src/ft_d.c
@@ -6,7 +6,7 @@
 /*   By: tstephan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 15:26:07 by tstephan          #+#    #+#             */
-/*   Updated: 2024/11/20 16:42:16 by tstephan         ###   ########.fr       */
+/*   Updated: 2024/11/20 17:56:26 by tstephan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,12 +29,17 @@ static int	ft_countdichar(int d)
 	return (len);
 }
 
-static void	ft_putdioptions(int d, int fd, int count)
+static void	ft_putdioptions(int d, int fd, int count, int plus)
 {
 	if (d < 0)
 	{
 		count++;
 		ft_putchar_fd('-', fd);
+	}
+	else if (d >= 0 && plus)
+	{
+		count++;
+		ft_putchar_fd('+', fd);
 	}
 	while (count-- > 0)
 		ft_putchar_fd('0', fd);
@@ -63,13 +68,14 @@ int	ft_putdioptions_fd(int d, struct s_option options, int fd)
 	caracs.padLeft = options.minus;
 	if (caracs.padLeft)
 	{	
-		ft_putdioptions(d, fd, options.precision - caracs.size);
+		ft_putdioptions(d, fd, options.precision - caracs.size, options.plus);
 		ft_addchar(caracs.pad, options.zero);
 	}
 	else
 	{	
 		ft_addchar(caracs.pad, options.zero);
-		ft_putdioptions(d, fd, options.precision - caracs.size);
+		ft_putdioptions(d, fd, options.precision - caracs.size, options.plus);
 	}
-	return (ft_getmax(3, caracs.size, options.width, options.precision + (1 && d < 0)));
+	return (ft_getmax(3, caracs.size, options.width, options.precision + (1 && d < 0))
+	 + (options.plus && d >= 0));
 }
