a079ce0f9db6d167b2432a611dd86440553c01af
skydogzz <thomas.stephan@live.fr>
2024-11-21 00:45:32 +0100

    50 tests ???????

--- ft_printf2/src/ft_xX.c
+++ ft_printf2/src/ft_xX.c
@@ -6,7 +6,7 @@
 /*   By: tstephan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 17:09:32 by tstephan          #+#    #+#             */
-/*   Updated: 2024/11/20 18:56:56 by tstephan         ###   ########.fr       */
+/*   Updated: 2024/11/21 00:38:23 by skydogzz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	ft_countxchar(unsigned int x)
 {
-	int		len;
+	int	len;
 
 	if (x == 0)
 		return (1);
@@ -27,83 +27,56 @@ int	ft_countxchar(unsigned int x)
 	return (len);
 }
 
-void	ft_putxnbr(unsigned int x, int maj)
+void	ft_putxnbr_fd(unsigned int x, int maj, int fd)
 {
 	char	letter;
 
 	if (x / 16 > 0)
-		ft_putxnbr(x / 16, maj);
+		ft_putxnbr_fd(x / 16, maj, fd);
 	if (x % 16 <= 9)
 		letter = x % 16 + '0';
 	else
-		letter = x % 16 + 'a' - 10 - maj * 32;
-	write(1, &letter, 1);
+		letter = x % 16 + (maj ? 'A' : 'a') - 10;
+	write(fd, &letter, 1);
 }
 
-void	ft_putxnbr_lim(unsigned int x, int maj, int lim)
-{
-	char	letter;
-
-	if (!lim)
-		return ;
-	if (x / 16 > 0)
-		ft_putxnbr_lim(x / 16, maj, lim - 1);
-	if (x % 16 <= 9)
-		letter = x % 16 + '0';
-	else
-		letter = x % 16 + 'a' - 10 - maj * 32;
-	write(1, &letter, 1);
-}
-
-int	ft_putxoptions_fd(unsigned int u, struct s_option options, int fd)
+int	ft_putxoptions_fd(unsigned int x, struct s_option options, int fd)
 {
 	struct s_carac	caracs;
 	int				maj;
 
 	ft_initcaracs(&caracs);
-	caracs.size = ft_countxchar(u);
-	caracs.pad = options.width - caracs.size;
+	caracs.size = (x == 0 && options.precision == 0) ? 0 : ft_countxchar(x);
+	caracs.pad = options.width - ft_getmax(2, caracs.size, options.precision)
+		- ((options.sharp && x != 0) ? 2 : 0);
+	caracs.pad = (caracs.pad < 0) ? 0 : caracs.pad;
 	caracs.padleft = options.minus;
-	maj = 0;
-	maj = options.specifier == 'X';
-	if (options.precision >= 0 && options.precision > caracs.size)
-	{
+	maj = (options.specifier == 'X');
+
+	// Remplissage à gauche
+	if (!caracs.padleft && (!options.zero || options.precision >= 0))
+		ft_addchar(caracs.pad, 0);
+
+	// Préfixe pour le sharp
+	if (options.sharp && x != 0)
+		ft_putstr_fd(maj ? "0X" : "0x", fd);
+
+	// Remplissage de précision
+	if (options.precision > caracs.size)
 		ft_addchar(options.precision - caracs.size, 1);
-		if (options.sharp && u != 0)
-		{
-			if (maj)
-				ft_putstr_fd("0X", 1);
-			else
-				ft_putstr_fd("0x", 1);
-		}
-		ft_putxnbr(u, maj);
-		return (ft_getmax(3, caracs.size, options.width, options.precision));
-	}
+
+	// Remplissage avec zéro pour le flag `0` uniquement si pas de précision
+	if (!caracs.padleft && options.zero && options.precision < 0)
+		ft_addchar(caracs.pad, 1);
+
+	// Affichage du nombre, sauf si taille = 0 et précision explicitement définie comme 0
+	if (caracs.size > 0)
+		ft_putxnbr_fd(x, maj, fd);
+
+	// Remplissage à droite
 	if (caracs.padleft)
-	{
-		if (options.sharp && u != 0)
-		{
-			if (maj)
-				ft_putstr_fd("0X", 1);
-			else
-				ft_putstr_fd("0x", 1);
-		}
-		ft_putxnbr(u, maj);
-		ft_addchar(caracs.pad, options.zero);
-	}
-	else
-	{
-		if (options.sharp && u != 0)
-		{
-			if (maj)
-				ft_putstr_fd("0X", 1);
-			else
-				ft_putstr_fd("0x", 1);
-		}
-		ft_addchar(caracs.pad, options.zero);
-		ft_putxnbr(u, maj);
-	}
-	return (ft_getmax(2, caracs.size, options.width)
-		+ (options.sharp && u != 0) * 2);
-	(void) fd;
+		ft_addchar(caracs.pad, 0);
+
+	return (ft_getmax(3, caracs.size, options.width, options.precision)
+		+ ((options.sharp && x != 0) ? 2 : 0));
 }
