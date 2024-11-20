ee5d1972599fc4a21d0f303398113ec14af88c64
skydogzz <thomas.stephan@live.fr>
2024-11-21 00:23:39 +0100

    will i succeed ?

--- ft_printf2/src/ft_d.c
+++ ft_printf2/src/ft_d.c
@@ -6,7 +6,7 @@
 /*   By: tstephan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 15:26:07 by tstephan          #+#    #+#             */
-/*   Updated: 2024/11/21 00:08:49 by skydogzz         ###   ########.fr       */
+/*   Updated: 2024/11/21 00:23:03 by skydogzz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,24 +68,30 @@ int	ft_putdioptions_fd(int d, struct s_option options, int fd)
 	struct s_carac	caracs;
 
 	if (d == 0 && options.precision == 0)
-		return (0);
+	{
+		ft_addchar(options.width, 0);
+		return (options.width);
+	}
 	ft_initcaracs(&caracs);
 	caracs.size = ft_countdichar(d);
-	caracs.pad = options.width - caracs.size;
+	caracs.pad = options.width - ft_getmax(3, caracs.size, options.precision)
+		- (options.plus && d >= 0) - (options.space && d >= 0);
+	if (d < 0)
+		caracs.pad--;
 	caracs.padleft = options.minus;
 	if (caracs.padleft)
-	{	
+	{
 		ft_putdioptions(d, fd, options.precision - caracs.size, options.plus,
-			options.space);
+				  options.space);
 		ft_addchar(caracs.pad, options.zero);
 	}
 	else
 	{	
 		ft_addchar(caracs.pad, options.zero);
 		ft_putdioptions(d, fd, options.precision - caracs.size, options.plus,
-			options.space);
+				  options.space);
 	}
 	return (ft_getmax(3, caracs.size, options.width, options.precision
-			+ (1 && d < 0)) + (options.plus && d >= 0)
-		+ (options.space && d >= 0));
+				   + (1 && d < 0)) + (options.plus && d >= 0)
+	+ (options.space && d >= 0));
 }
