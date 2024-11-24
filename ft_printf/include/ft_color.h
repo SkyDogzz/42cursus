8374e26f4a017f6e9eb3a4aafabb94b4eced3dad
skydogzz <thomas.stephan@live.fr>
2024-11-24 14:42:14 +0100

    remove useless tab

--- ft_printf/include/ft_color.h
+++ ft_printf/include/ft_color.h
@@ -6,20 +6,20 @@
 /*   By: skydogzz </var/spool/mail/skydogzz>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 13:53:04 by skydogzz          #+#    #+#             */
-/*   Updated: 2024/11/24 14:39:49 by skydogzz         ###   ########.fr       */
+/*   Updated: 2024/11/24 14:41:55 by skydogzz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_COLOR_H
 # define FT_COLOR_H
 
-# define COLOR_RESET		"\x1B[0m"
+# define COLOR_RESET	"\x1B[0m"
 # define COLOR_RED		"\x1B[31m"
-# define COLOR_GREEN		"\x1B[32m"
+# define COLOR_GREEN	"\x1B[32m"
 # define COLOR_YELLOW	"\x1B[33m"
 # define COLOR_BLUE		"\x1B[34m"
 # define COLOR_MAGENTA	"\x1B[35m"
 # define COLOR_CYAN		"\x1B[36m"
-# define COLOR_WHITE		"\x1B[37m"
+# define COLOR_WHITE	"\x1B[37m"
 
 #endif
