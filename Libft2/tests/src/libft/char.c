fa872849b3b970fe5890e97dc1d70da84c76a6ad
SkyDogzz <thomas.stephan@live.fr>
2024-11-26 18:27:51 +0100

    test all ft_is/ft_to

--- Libft2/tests/src/libft/char.c
+++ Libft2/tests/src/libft/char.c
@@ -6,15 +6,16 @@
 /*   By: tstephan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/26 17:22:40 by tstephan          #+#    #+#             */
-/*   Updated: 2024/11/26 18:12:00 by tstephan         ###   ########.fr       */
+/*   Updated: 2024/11/26 18:27:14 by tstephan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/test.h"
+#include "../../../include/libft.h"
 #include <ctype.h>
 #include <limits.h>
 
-int	test_charis(int (*f1)(int), int (*f2)(int), int fd)
+int	test_charis(const char *name, int (*f1)(int), int (*f2)(int), int fd)
 {
 	int	c;
 	int	r1;
@@ -27,9 +28,9 @@ int	test_charis(int (*f1)(int), int (*f2)(int), int fd)
 	{
 		r1 = f1(c);
 		r2 = f2(c);
-		if (r1 && 1 != r2 && 1)
+		if ((r1 && 1) != (r2 && 1))
 		{
-			ft_dprintf(fd, "Error when c = %d, expected %d, got %d\n", c, r1, r2);
+			ft_dprintf(fd, "Error when c = %d, expected %d, got %d in %s\n", c, r1, r2, name);
 			errors++;
 		}
 		c++;
@@ -42,10 +43,19 @@ int test_char(void)
 	int	errors;
 	int	fd;
 
-	fd = open("log/char1.log", O_CREAT|O_RDWR, 0644);
-	ft_printf("fd = %d\n", fd);
+	fd = open("log/char.log", O_CREAT|O_RDWR, 0644);
 	errors = 0;
-	errors = test_charis(&isalpha, &ft_isalpha, fd);
+	errors += test_charis("ft_isalnum", &isalnum, &ft_isalnum, fd);
+	errors += test_charis("ft_isalpha", &isalpha, &ft_isalpha, fd);
+	// errors += test_charis("ft_isascii", &isascii, &ft_isascii, fd);
+	errors += test_charis("ft_isblank", &isblank, &ft_isblank, fd);
+	errors += test_charis("ft_isdigit", &isdigit, &ft_isdigit, fd);
+	errors += test_charis("ft_islower", &islower, &ft_islower, fd);
+	errors += test_charis("ft_isprint", &isprint, &ft_isprint, fd);
+	errors += test_charis("ft_isspace", &isspace, &ft_isspace, fd);
+	errors += test_charis("ft_isupper", &isupper, &ft_isupper, fd);
+	errors += test_charis("ft_toupper", &toupper, &ft_toupper, fd);
+	errors += test_charis("ft_tolower", &tolower, &ft_tolower, fd);
 	close(fd);
 	return (errors);
 }
