81ff9f2f5bd8fa8cafc181500e98efbdac93e3ca
SkyDogzz <thomas.stephan@live.fr>
2024-11-26 16:41:44 +0100

    remove lsp errors

--- Libft2/include/libft.h
+++ Libft2/include/libft.h
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 14:07:50 by marvin            #+#    #+#             */
-/*   Updated: 2024/11/26 14:41:56 by tstephan         ###   ########.fr       */
+/*   Updated: 2024/11/26 16:41:11 by tstephan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -96,10 +96,10 @@ typedef struct s_list
 }	t_list;
 
 t_list	*ft_lstnew(void *content);
-void	ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstadd_front(t_list **lst, t_list *newl);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
-t_list	*ft_lstadd_back(t_list **lst, t_list *new);
+t_list	*ft_lstadd_back(t_list **lst, t_list *newl);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
 void	ft_lstclear(t_list **lst, void (*del)(void *));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
