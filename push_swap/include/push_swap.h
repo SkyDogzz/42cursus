65a0e5279c10fbb2810351e17e848ee876535c61
SkyDogzz <thomas.stephan@live.fr>
2024-12-09 16:45:55 +0100

    parse and create base function (push / swap / rotate / rev rotate)

--- push_swap/include/push_swap.h
+++ push_swap/include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: tstephan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 14:37:37 by tstephan          #+#    #+#             */
-/*   Updated: 2024/12/09 15:43:30 by tstephan         ###   ########.fr       */
+/*   Updated: 2024/12/09 16:44:20 by tstephan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,9 @@ void	print(void *content);
 void	print_tlist(t_list *list);
 t_list	*parse_input(char *args);
 void	free_list(t_list *list);
+void	revrotate(t_list **list);
+void	rotate(t_list **list);
+void	push(t_list **l1, t_list **l2);
+void	swap(t_list **list);
 
 #endif
