ad2bf3e9ceb348f0ae35d6c2006dab6474292376
SkyDogzz <thomas.stephan@live.fr>
2024-12-09 20:31:21 +0100

    a

--- pipex/include/pipex.h
+++ pipex/include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: tstephan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 18:18:29 by tstephan          #+#    #+#             */
-/*   Updated: 2024/12/09 18:48:43 by tstephan         ###   ########.fr       */
+/*   Updated: 2024/12/09 20:08:22 by tstephan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,19 @@
 
 # include "../libft/include/libft.h"
 # include "../libft/include/ft_printf.h"
+# include <fcntl.h>
+
+typedef struct s_cmd
+{
+	int		fd;
+	char	**args;
+}	t_cmd;
 
 typedef struct s_pipex
 {
 	char	*infile;
 	char	*outfile;
-	char	**cmds;
+	t_cmd	*cmds;
 	int		cmds_num;
 }	t_pipex;
 
--- pipex/src/main.c
+++ pipex/src/main.c
@@ -6,17 +6,21 @@
 /*   By: tstephan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 18:17:42 by tstephan          #+#    #+#             */
-/*   Updated: 2024/12/09 19:22:49 by tstephan         ###   ########.fr       */
+/*   Updated: 2024/12/09 20:20:32 by tstephan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/pipex.h"
 
-void	free_tab(char **cmds, int index)
+void	free_cmd(t_cmd *cmds, int index)
 {
 	while (index)
-		free(cmds[--index]);
+	{
+		free(cmds[--index].args[0]);
+		free(cmds[index].args);
+	}
 	free(cmds);
+	(void) index;
 }
 
 void	parse_args(t_pipex *pipex, int argc, char *argv[])
@@ -26,15 +30,17 @@ void	parse_args(t_pipex *pipex, int argc, char *argv[])
 	pipex->infile = ft_strdup(argv[1]);
 	pipex->outfile = ft_strdup(argv[argc - 1]);
 	pipex->cmds_num = argc - 3;
-	pipex->cmds = (char **)malloc(sizeof(char *) * pipex->cmds_num);
+	pipex->cmds = (t_cmd *)malloc(sizeof(t_cmd) * pipex->cmds_num);
 	if (!pipex->cmds)
 		return ;
 	pos = 0;
 	while (pos < pipex->cmds_num)
 	{
-		pipex->cmds[pos] = ft_strdup(argv[pos + 2]);
-		if (!pipex->cmds[pos])
-			free_tab(pipex->cmds, pos);
+		pipex->cmds[pos].args = (char **)malloc(sizeof(char *) * (1 + 1));
+		pipex->cmds[pos].args[0] = ft_strjoin("/bin/", ft_strdup(argv[pos + 2]));
+		pipex->cmds[pos].args[1] = NULL;
+		if (!pipex->cmds[pos].args[0])
+			free_cmd(pipex->cmds, pos);
 		pos++;
 	}
 	(void) pos;
@@ -44,7 +50,7 @@ void	free_full(t_pipex *pipex)
 {
 	free(pipex->infile);
 	free(pipex->outfile);
-	free_tab(pipex->cmds, pipex->cmds_num);
+	free_cmd(pipex->cmds, pipex->cmds_num);
 }
 
 void	print_pipex(t_pipex pipex)
@@ -55,7 +61,7 @@ void	print_pipex(t_pipex pipex)
 	pos = 0;
 	while (pos < pipex.cmds_num)
 	{
-		ft_printf("cmd%d = %s ", pos, pipex.cmds[pos]);
+		ft_printf("cmd%d = %s ", pos, pipex.cmds[pos].args[0]);
 		pos++;
 	}
 	ft_printf("\n");
@@ -65,7 +71,7 @@ void	print_pipex(t_pipex pipex)
 
 int main(int argc, char *argv[], char *env[])
 {
-	t_pipex	pipex;
+	t_pipex	pipex;    
 	if (argc < 4)
 	{
 		ft_dprintf(2, "Usage %s infile cmd1 cmd2 ... cmdn outfile\n", argv[0]);
@@ -76,8 +82,13 @@ int main(int argc, char *argv[], char *env[])
 	{
 		perror("Error");
 		exit(0);
-	}	
+	}
+	int in = open(pipex.infile, O_RDONLY);
+	int out = open(pipex.outfile, O_WRONLY);
 	print_pipex(pipex);
+	dup2(in, 0);
+	dup2(out, 1);
+	execve(pipex.cmds->args[0], pipex.cmds->args, env);
 	free_full(&pipex);
 	(void) env;
 }
