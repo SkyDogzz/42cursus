02483538fb84771beb2300bdf9ae3ddaecb88fa2
SkyDogzz <thomas.stephan@live.fr>
2024-12-12 13:41:46 +0100

    finish parsing

--- push_swap2/src/parse.c
+++ push_swap2/src/parse.c
@@ -6,7 +6,7 @@
 /*   By: skydogzz <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/12 04:26:37 by skydogzz          #+#    #+#             */
-/*   Updated: 2024/12/12 05:17:48 by skydogzz         ###   ########.fr       */
+/*   Updated: 2024/12/12 13:40:32 by skydogzz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,32 @@ void	strcat_space(char *dest, const char *src)
 		*dest++ = *src++;
 	*dest++ = ' ';
 	*dest = 0;
+}
+
+t_bool	args_valid(const char *args)
+{
+	while (*args)
+	{
+		if (!ft_isdigit(*args) && !ft_isspace(*args))
+			return (FALSE);
+		args++;
+	}
+	return (TRUE);
+}
+
+void	get_args(t_stack *stack, const char *s)
+{
+	push(stack, ft_atoi(s));
+	while (*s)
+	{
+		while (ft_isdigit(*s))
+			s++;
+		while (ft_isspace(*s))
+			s++;
+		if (*s == 0)
+			return ;
+		push(stack, ft_atoi(s));
+	}
 }
 
 t_stack	*parse_args(t_stack *stack, int argc, char *argv[])
@@ -36,6 +62,9 @@ t_stack	*parse_args(t_stack *stack, int argc, char *argv[])
 	pos = 0;
 	while (++pos < argc)
 		strcat_space(full, argv[pos]);
+	if (!args_valid(full))
+		return (NULL);
+	get_args(stack, full);
 	free(full);
 	return (stack);
 }
