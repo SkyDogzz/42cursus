39629212b6c4d0d6d060d9b75360be8d50dae794
SkyDogzz <thomas.stephan@live.fr>
2024-12-18 15:22:58 +0100

    ngl gonna try to start philo

--- push_swap3/src/sort.c
+++ push_swap3/src/sort.c
@@ -6,7 +6,7 @@
 /*   By: tstephan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 17:33:22 by tstephan          #+#    #+#             */
-/*   Updated: 2024/12/17 20:46:49 by tstephan         ###   ########.fr       */
+/*   Updated: 2024/12/18 15:08:50 by tstephan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,28 @@ void	push_all(t_inst *inst, t_stack *stack, t_stack *temp)
 	}
 }
 
+void	get_2(t_inst *inst, t_stack *stack, t_stack *temp)
+{
+	int		index;
+	t_node	*node;
+
+	node = temp->top;
+	index = 0;
+	while (index++ < 2)
+	{
+		execpa(inst, stack, pop(temp));
+		node = temp->top;
+	}
+}
+
 void	get_borne(int *min, int *max, t_stack *stack)
 {
 	t_node	*node;
 
 	if (min != 0)
-		*min = INT_MIN;
+		*min = INT_MAX;
 	if (max != 0)
-		*max = INT_MAX;
+		*max = INT_MIN;
 	node = stack->top;
 	if (!node)
 		return ;
@@ -45,14 +59,82 @@ void	get_borne(int *min, int *max, t_stack *stack)
 	}
 }
 
-void	move_cheapest(t_inst *inst, t_stack *stack, t_stack *temp)
+int	get_index(int value, t_stack *stack)
 {
+	t_node	*node;
+	int		pos;
+
+	node = stack->top;
+	if (!node)
+		return (-1);
+	pos = 0;
+	while (pos < (int)stack->size)
+	{
+		if (node->value == value)
+			return (pos);
+		node = node->next;
+		pos++;
+	}
+	return (-1);
+}
+
+int	find_cheapest(t_stack *stack, t_stack *temp)
+{
+	return (0);
+	(void) stack;
+	(void) temp;
+}
+
+void	get_to_top_a(t_inst *inst, t_stack *stack , int index)
+{
+	if (index < (int)stack->size / 2)
+	{
+		while (index--)
+			execrra(inst, stack);
+	}
+	else
+	{
+		index = stack->size - index;
+		while (index--)
+			execra(inst, stack);
+	}
+}
+
+void	get_to_top_b(t_inst *inst, t_stack *stack , int index)
+{
+	if (index < (int)stack->size / 2)
+	{
+		while (index--)
+			execrra(inst, stack);
+	}
+	else
+	{
+		index = stack->size - index;
+		while (index--)
+			execra(inst, stack);
+	}
+}
+void	move_cheapest(t_inst *inst, t_stack *stack, t_stack *temp, int cheapest)
+{
+	int	max;
+
+	get_to_top_b(inst, temp, cheapest);
+	get_borne(NULL, &max, stack);
+	get_to_top_a(inst, stack, get_index(max, stack));
 	execpa(inst, stack, pop(temp));
 }
 
 void	sort_stack(t_inst *inst, t_stack *stack, t_stack *temp)
 {
+	int	cheapest;
+
+	if (!stack->top || !stack->top->next)
+		return ;
 	push_all(inst, stack, temp);
+	get_2(inst, stack, temp);
 	while (temp->top)
-		move_cheapest(inst, stack, temp);
+	{
+		cheapest = find_cheapest(stack, temp);
+		move_cheapest(inst, stack, temp, cheapest);
+	}
 }
